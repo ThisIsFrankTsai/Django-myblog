2090ced2964846a32707d98bc10a950625c46ae1
ThisIsFrankTsai <vg8rwb@gmail.com>
2023-07-26 21:59:12 +0800

    add homework

--- mainsite/models.py
+++ mainsite/models.py
@@ -5,6 +5,8 @@ from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.CharField(max_length=200)
+    abstract = models.TextField()
+    pic_url= models.URLField(max_length=250)
     body = models.TextField()
     pub_date = models.DateTimeField(default=timezone.now)
 
